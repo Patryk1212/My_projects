5d251b7d36ab1b76a6df4245a6efee36130b09cc
Patryk Ostrowski <patryk2.ostrowski@live.uwe.ac.uk>
2019-01-28 23:23:45 +0000

    ready to code

--- Source/Balltastic.cpp
+++ Source/Balltastic.cpp
@@ -7,66 +7,38 @@
 
 #include "Balltastic.h"
 
-/**
-*   @brief   Default Constructor.
-*   @details Consider setting the game's width and height
-             and even seeding the random number generator.
-*/
 Balltastic::Balltastic()
 {
     game_width = 1024;
     game_height = 768;
-    std::srand(time(NULL));
 }
 
-/**
-*   @brief   Destructor.
-*   @details Remove any non-managed memory and callbacks.
-*/
 Balltastic::~Balltastic()
 {
     this->inputs->unregisterCallback(key_callback_id);
     this->inputs->unregisterCallback(mouse_callback_id);
 
-    if (background)
-    {
-        delete background;
-        background = nullptr;
-    }
-
-    if (ball)
-    {
-        delete ball;
-        ball = nullptr;
-    }
 }
 
-/**
-*   @brief   Initialises the game.
-*   @details The game window is created and all assets required to
-			 run the game are loaded. The keyHandler and clickHandler
-			 callback should also be set in the initialise function.
-*   @return  True if the game initialised correctly.
-*/
 bool Balltastic::init()
 {
-    // initialises the graphics API, check to make sure it was a success
     if (!initAPI())
     {
         return false;
     }
 
-    // disable sprite batching for simplicity
-    renderer->setSpriteMode(ASGE::SpriteSortMode::IMMEDIATE);
+
+
     renderer->setWindowTitle("Balltastic Fun");
     renderer->setClearColour(ASGE::COLOURS::BLACK);
-
-    toggleFPS();
 
     ball = renderer->createRawSprite();
     ball->loadTexture("data/images/8bit_ball.png");
     ball->width(16);
     ball->height(16);
+
+
+    renderer->setSpriteMode(ASGE::SpriteSortMode::IMMEDIATE);
 
     // input handling functions
     key_callback_id = inputs->addCallbackFnc(
@@ -81,16 +53,6 @@ bool Balltastic::init()
     return true;
 }
 
-/**
-*   @brief   Processes any key inputs
-*   @details This function is added as a callback to handle the game's
-			 keyboard input. For this game, calls to this function
-			 are thread safe, so you may alter the game's state as you
-			 see fit.
-*   @param   data The event data relating to key input.
-*   @see     KeyEvent
-*   @return  void
-*/
 void Balltastic::keyHandler(const ASGE::SharedEventData data)
 {
     auto key = static_cast<const ASGE::KeyEvent*>(data.get());
@@ -107,16 +69,6 @@ void Balltastic::keyHandler(const ASGE::SharedEventData data)
     }
 }
 
-/**
-*   @brief   Processes any key inputs
-*   @details This function is added as a callback to handle the game's
-		     mouse button input. For this game, calls to this function
-             are thread safe, so you may alter the game's state as you
-             see fit.
-*   @param   data The event data relating to key input.
-*   @see     ClickEvent
-*   @return  void
-*/
 void Balltastic::clickHandler(const ASGE::SharedEventData data)
 {
     auto click = static_cast<const ASGE::ClickEvent*>(data.get());
@@ -125,14 +77,6 @@ void Balltastic::clickHandler(const ASGE::SharedEventData data)
 }
 
 
-
-/**
-*   @brief   Updates the scene
-*   @details Prepares the renderer subsystem before drawing the
-		     current frame. Once the current frame is has finished
-		     the buffers are swapped accordingly and the image shown.
-*   @return  void
-*/
 void Balltastic::update(const ASGE::GameTime& us)
 {
     if (!in_menu)
@@ -163,23 +107,11 @@ void Balltastic::update(const ASGE::GameTime& us)
         }
 
 
-
-        // move the ball in the upward direction, say 500pixels/sec?
-        // delta time is measured in milliseconds, so divide by 1000 and multiply
-        // use us for the delta measurements
-
         ball->xPos(x_pos);
         ball->yPos(y_pos);
     }
 }
 
-/**
-*   @brief   Renders the scene
-*   @details Renders all the game objects to the current frame.
-	         Once the current frame is has finished the buffers are
-			 swapped accordingly and the image shown.
-*   @return  void
-*/
 void Balltastic::render(const ASGE::GameTime &)
 {
     renderer->setFont(0);
